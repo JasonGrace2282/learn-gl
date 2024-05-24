125f022921bddcf7de22b23d485134b6223f7d70
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-05-24 19:21:19 -0400

    remove comments

--- src/main.cpp
+++ src/main.cpp
@@ -134,13 +134,6 @@ int main()
 
       glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
       glEnableVertexAttribArray(0);
-
-      // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-      // glBindBuffer(GL_ARRAY_BUFFER, 0); 
-
-      // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-      // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-      // glBindVertexArray(0); 
     }
 
 
@@ -156,7 +149,6 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // draw our first triangle
         const float time = glfwGetTime();
         const float green = sin(time) / 2.0f + 0.5f;
         int colorUniformLocation = glGetUniformLocation(shaderProgram, "color");
